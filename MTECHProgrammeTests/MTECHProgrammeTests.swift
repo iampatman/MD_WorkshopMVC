98f82453cc002b5b819a20bdef5c6c895399399d
Nguyen Trung <nguyentrung0904@gmail.com>
2016-04-15 14:49:59 +0800

    Initial Commit

---
+++ MTECHProgrammeTests/MTECHProgrammeTests.swift
@@ -0,0 +1,36 @@
+//
+//  MTECHProgrammeTests.swift
+//  MTECHProgrammeTests
+//
+//  Created by Nguyen Bui An Trung on 15/4/16.
+//  Copyright © 2016 Nguyen Bui An Trung. All rights reserved.
+//
+
+import XCTest
+@testable import MTECHProgramme
+
+class MTECHProgrammeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
